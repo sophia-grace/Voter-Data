9ce24d39975d27dc14e4bb86fd739981690f8662
Sophia Trump <strump@stratos.cs.brynmawr.edu>
2018-11-29 15:19:54 -0500

    Backbone code

---
+++ Vote.java
@@ -0,0 +1,17 @@
+/*
+Name: Sophia Trump
+File: Vote.java
+Description: Counts the number of votes cast for Democratic and Republican
+             governors in the PA 2018 election.
+Date: 29/11/18
+*/
+
+import java.io.*;
+
+public class Vote {
+
+
+  public static void main(String[] args) throws IOException {
+
+  }
+}
