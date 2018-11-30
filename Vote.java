8307ab40f76ab25ab37c125f73bb3be3f2ac7b1e
Sophia Trump <strump@stratos.cs.brynmawr.edu>
2018-11-30 18:46:04 -0500

    Removed unused code

--- Vote.java
+++ Vote.java
@@ -20,8 +20,7 @@ public class Vote {
     // split the relevant lines and store their vote sums in a Tuple
     // (the lines are organized s.t. columns are separated by semicolons)
     // source for if/else idea: https://stackoverflow.com/questions/38021061/how-to-use-if-else-logic-in-java-8-stream-foreach
-  //  Tuple<Integer[], Integer[]> result = new Tuple<Integer[],Integer[]>(0,0);
-  //  UnaryOperator<Integer> add = x -> x + x;
+    // source for array wrapper idea: https://stackoverflow.com/questions/31851892/java-changing-the-value-of-a-final-variable-from-within-a-lambda
     final Integer[] sum = new Integer[2];
     sum[0] = 0;
     sum[1] = 0;
@@ -29,23 +28,15 @@ public class Vote {
          .filter(e -> e[2].equals("Governor"))
          .forEach(e -> {
             if(e[4].equals("Democratic")) {
-              //  demVotes += Integer.parseInt(e[6]);
-              // System.out.println("D");
-              // System.out.println(Integer.parseInt(e[6]));
-            //  result.changeFirst(add.apply(Integer.parseInt(e[6])));
-              //System.out.println(result.fst);
               sum[0] += Integer.parseInt(e[6]);
             }
             else if(e[4].equals("Republican")) {
-              //  repVotes += Integer.parseInt(e[6]);
-              // System.out.println("R");
-              //result.changeSecond(Integer.parseInt(e[6]));
               sum[1] += Integer.parseInt(e[6]);
             }
          });
 
     // output the result
-    System.out.println("Democratic votes: " + " " + sum[0]);
-    System.out.println("Republican votes: " + " " + sum[1]);
+    System.out.println("Democratic votes: " + sum[0]);
+    System.out.println("Republican votes: " + sum[1]);
   }
 }
