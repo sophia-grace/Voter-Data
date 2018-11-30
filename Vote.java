4c281b45aad38ea9084dcd15cc6a07914a3df5f7
Sophia Trump <strump@stratos.cs.brynmawr.edu>
2018-11-30 17:05:31 -0500

    More confusion but about to try if/else in lambda

--- Vote.java
+++ Vote.java
@@ -17,6 +17,16 @@ public class Vote {
     return relevantStringArray.mapToInt(e -> Integer.parseInt(e[6])).sum();
   }
 
+  // used to group the elements of the stream
+  public static boolean isDemocratic(String[] ln) {
+    if(ln[4].equals("Democratic")) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
   public static void main(String[] args) throws IOException {
     // produce a stream of strings where each string is a line from the file
     Stream<String> lines = new BufferedReader(new FileReader("voterdata.csv")).lines();
@@ -31,9 +41,12 @@ public class Vote {
           .filter(e -> e[2].equals("Governor"))
           .filter(e -> (e[4].equals("Democratic")) || (e[4].equals("Republican"))); */
 
-    Tuple<Stream<String[]>, Stream<String[]>> demrepResult = new Tuple
-    (lines.map(e -> e.split(";")).filter(e -> e[2].equals("Governor")).filter(e -> (e[4].equals("Democratic"))
-    , lines.map(e -> e.split(";")).filter(e -> e[2].equals("Governor")).filter(e -> (e[4].equals("Republican")))));
+
+    Map<Tuple, List<String[]>> relevantStringArray = lines
+            .map(e -> e.split(";"))
+            .filter(e -> e[2].equals("Governor"))
+            .collect(Collectors.groupingBy(e -> new Tuple(e[4], e[4])));
+    relevantStringArray.forEach((k,v) -> System.out.println(k.fst + "       " + k.snd));
     // create tuple with dem and rep lines
   //  Tuple<Stream<String[]>, Stream<String[]>> demrepResult = new Tuple(relevantStringArray.filter(e -> e[4].equals("Democratic")), relevantStringArray.filter(e -> e[4].equals("Republican")));
 
