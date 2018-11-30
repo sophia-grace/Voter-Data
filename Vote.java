615a0138cdbb99822fc3eb04f82c8366dda376a9
Sophia Trump <strump@stratos.cs.brynmawr.edu>
2018-11-30 18:10:48 -0500

    Working for Dem and Rep votes (w/ final array wrapper)

--- Vote.java
+++ Vote.java
@@ -13,52 +13,39 @@ import java.util.function.*;
 
 public class Vote {
 
-  // returns the sum of the votes (index 6) for the given input
-  public static int countVotes(Stream<String[]> relevantStringArray) {
-    return relevantStringArray.mapToInt(e -> Integer.parseInt(e[6])).sum();
-  }
-
-  // used to group the elements of the stream
-  public static boolean isDemocratic(String[] ln) {
-    if(ln[4].equals("Democratic")) {
-      return true;
-    }
-    else {
-      return false;
-    }
-  }
-
   public static void main(String[] args) throws IOException {
     // produce a stream of strings where each string is a line from the file
     Stream<String> lines = new BufferedReader(new FileReader("voterdata.csv")).lines();
 
-    // get the relevant lines
-    // and split them by constituent pieces
+    // split the relevant lines and store their vote sums in a Tuple
     // (the lines are organized s.t. columns are separated by semicolons)
-
-  //  Tuple<Integer, Integer> demrepResult;
-  /*  Stream<String[]> relevantStringArray = lines
-          .map(e -> e.split(";"))
-          .filter(e -> e[2].equals("Governor"))
-          .filter(e -> (e[4].equals("Democratic")) || (e[4].equals("Republican"))); */
-
-    Tuple<Integer, Integer> result = new Tuple<Integer,Integer>(0,0);
+    // source for if/else idea: https://stackoverflow.com/questions/38021061/how-to-use-if-else-logic-in-java-8-stream-foreach
+  //  Tuple<Integer[], Integer[]> result = new Tuple<Integer[],Integer[]>(0,0);
+  //  UnaryOperator<Integer> add = x -> x + x;
+    final Integer[] sum = new Integer[2];
+    sum[0] = 0;
+    sum[1] = 0;
     lines.map(e -> e.split(";"))
          .filter(e -> e[2].equals("Governor"))
          .forEach(e -> {
             if(e[4].equals("Democratic")) {
               //  demVotes += Integer.parseInt(e[6]);
               // System.out.println("D");
-              result.changeFirst(result.fst + Integer.parseInt(e[6]));
+              // System.out.println(Integer.parseInt(e[6]));
+            //  result.changeFirst(add.apply(Integer.parseInt(e[6])));
+              //System.out.println(result.fst);
+              sum[0] += Integer.parseInt(e[6]);
             }
             else if(e[4].equals("Republican")) {
               //  repVotes += Integer.parseInt(e[6]);
               // System.out.println("R");
-              result.changeSecond(result.snd + Integer.parseInt(e[6]));
+              //result.changeSecond(Integer.parseInt(e[6]));
+              sum[1] += Integer.parseInt(e[6]);
             }
          });
 
-    // create tuple with dem and rep lines
-    System.out.println(result.fst);
+    // output the result
+    System.out.println("Democratic votes: " + " " + sum[0]);
+    System.out.println("Republican votes: " + " " + sum[1]);
   }
 }
